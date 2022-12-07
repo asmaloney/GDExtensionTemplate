bb8736608927d713f8e9be28fbf090fb278ac18a
Andy Maloney <asmaloney@gmail.com>
2022-12-07 17:45:15 -0500

    Cleanup/modernize example (#8)
    Rename files, use an anonymous namespace, and remove header file.

--- src/Example.cpp
+++ src/Example.cpp
@@ -5,7 +5,7 @@
 #include "godot_cpp/core/class_db.hpp"
 #include "godot_cpp/variant/utility_functions.hpp"
 
-#include "example.h"
+#include "Example.h"
 
 ExampleRef::ExampleRef()
 {
--- src/RegisterExtension.cpp
+++ src/RegisterExtension.cpp
@@ -5,8 +5,32 @@
 #include "godot_cpp/core/defs.hpp"
 #include "godot_cpp/godot.hpp"
 
-#include "example.h"
-#include "register_types.h"
+#include "Example.h"
+
+namespace
+{
+   void initializeExtension( godot::ModuleInitializationLevel p_level )
+   {
+      if ( p_level != godot::MODULE_INITIALIZATION_LEVEL_SCENE )
+      {
+         return;
+      }
+
+      godot::ClassDB::register_class<ExampleRef>();
+      godot::ClassDB::register_class<ExampleMin>();
+      godot::ClassDB::register_class<Example>();
+      godot::ClassDB::register_class<ExampleVirtual>( true );
+      godot::ClassDB::register_abstract_class<ExampleAbstract>();
+   }
+
+   void uninitializeExtension( godot::ModuleInitializationLevel p_level )
+   {
+      if ( p_level != godot::MODULE_INITIALIZATION_LEVEL_SCENE )
+      {
+         return;
+      }
+   }
+}
 
 extern "C"
 {
@@ -17,32 +41,10 @@ extern "C"
    {
       godot::GDExtensionBinding::InitObject init_obj( p_interface, p_library, r_initialization );
 
-      init_obj.register_initializer( initialize_example_module );
-      init_obj.register_terminator( uninitialize_example_module );
+      init_obj.register_initializer( initializeExtension );
+      init_obj.register_terminator( uninitializeExtension );
       init_obj.set_minimum_library_initialization_level( godot::MODULE_INITIALIZATION_LEVEL_SCENE );
 
       return init_obj.init();
-   }
-}
-
-void initialize_example_module( godot::ModuleInitializationLevel p_level )
-{
-   if ( p_level != godot::MODULE_INITIALIZATION_LEVEL_SCENE )
-   {
-      return;
-   }
-
-   godot::ClassDB::register_class<ExampleRef>();
-   godot::ClassDB::register_class<ExampleMin>();
-   godot::ClassDB::register_class<Example>();
-   godot::ClassDB::register_class<ExampleVirtual>( true );
-   godot::ClassDB::register_abstract_class<ExampleAbstract>();
-}
-
-void uninitialize_example_module( godot::ModuleInitializationLevel p_level )
-{
-   if ( p_level != godot::MODULE_INITIALIZATION_LEVEL_SCENE )
-   {
-      return;
    }
 }
