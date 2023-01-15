baa85c845ea31d788084e3f9388b0d3fe5d00a67
Andy Maloney <asmaloney@gmail.com>
2023-01-15 17:35:46 -0500

    {cmake} Output version info for any tools we are using

--- cmake/ccache.cmake
+++ cmake/ccache.cmake
@@ -4,9 +4,19 @@
 find_program( CCACHE_PROGRAM ccache )
 
 if ( CCACHE_PROGRAM )
-    message( STATUS "Using ccache: ${CCACHE_PROGRAM}" )
+    # get version information
+    execute_process(
+        COMMAND "${CCACHE_PROGRAM}" --version
+        OUTPUT_VARIABLE CCACHE_VERSION
+    )
+
+    string( REGEX MATCH "[^\r\n]*" CCACHE_VERSION ${CCACHE_VERSION} )
+
+    message( STATUS "Using ccache: ${CCACHE_PROGRAM} (${CCACHE_VERSION})" )
 
     # Turn on ccache for all targets
     set( CMAKE_CXX_COMPILER_LAUNCHER "${CCACHE_PROGRAM}" )
     set( CMAKE_C_COMPILER_LAUNCHER "${CCACHE_PROGRAM}" )
+
+    unset( CCACHE_VERSION )
 endif()
